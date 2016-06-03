c29c221dec1d1575cabd6c1ecc2396701d8d99ca
Scott O'Neal <scottmod@gmail.com>
2016-06-03 09:57:55 -0400

    updated promise-breaker to use store mechanism, fixed tests. now the real refactor begins.

--- test/promise-breaker.js
+++ test/promise-breaker.js
@@ -1,14 +1,15 @@
 'use strict';
+/* jshint -W003 */
 
-const assert              = require('assert');
-const http                = require('http');
-const request             = require('request');
+const assert   = require('assert');
+const http     = require('http');
+const _        = require('lodash');
 
-const PromiseBreaker      = require('../../app/models/promise-breaker');
+const PromiseBreaker  = require('../lib/promise-breaker');
 
 describe('PromiseBreaker', function() {
-  var breaker, attributes;
-  var mockServer, timeout, port, status, message;
+  let breaker, attributes;
+  let mockServer, timeout, port, status, message;
 
   before(function() {
     port = 3034;
@@ -17,7 +18,8 @@ describe('PromiseBreaker', function() {
         res.writeHead(status, {'Content-Type': 'text/plain'});
         res.end(message);
       }), timeout);
-    })
+    });
+
     mockServer.listen(port);
     attributes = {
       url: 'http://127.0.0.1:' + port
@@ -28,11 +30,10 @@ describe('PromiseBreaker', function() {
     mockServer.close();
   });
 
-  beforeEach(function(done) {
+  beforeEach(function() {
     status = 200;
     timeout = 0;
     message = 'Oh yeah! it worked';
-    dbHelper.clearAllRecords(done);
   });
 
   // NOTE: this is really a private class. I tested it in order to know it worked easier
@@ -56,12 +57,13 @@ describe('PromiseBreaker', function() {
       timeout = 50;
       tripper = new PromiseBreaker.TimeTripper(attributes, timeout * 2);
       tripper.on('success', assertBody);
+      tripper.run();
+
       function assertBody(response) {
         assert.equal(response.statusCode, 200);
         assert.equal(response.body, 'Oh yeah! it worked');
         done();
-      };
-      tripper.run();
+      }
     });
 
     it('should emit a failure response when within time', function(done) {
@@ -69,17 +71,19 @@ describe('PromiseBreaker', function() {
       status = 400;
       tripper = new PromiseBreaker.TimeTripper(attributes, timeout * 2);
       tripper.on('failure', assertBody);
+      tripper.run();
+
       function assertBody(response) {
         assert.equal(response.statusCode, 400);
         done();
-      };
-      tripper.run();
+      }
     });
   });
 
   describe('when the breaker is below failure threshold', function() {
-    beforeEach(function() {
-      breaker = new PromiseBreaker('geo');
+    beforeEach(function(done) {
+      breaker  = new PromiseBreaker('geo');
+      breaker.setup().then(() => done());
     });
 
     it('allows requests to go through and responds with promiseness', function(done) {
@@ -126,8 +130,8 @@ describe('PromiseBreaker', function() {
         .catch(assertFailures);
 
       function assertFailures() {
-        circuitBreaker
-          .findByName(breaker.name)
+        breaker.store
+          .get(breaker.name)
           .then(function(record) {
             assert.equal(record.consec_faults, 1);
             assert.equal(record.fault_ct, 1);
@@ -142,16 +146,11 @@ describe('PromiseBreaker', function() {
 
   describe('when the breaker is tripped because of too many consecutive faults', function() {
     beforeEach(function(done) {
-      breaker = new PromiseBreaker('geo');
-      breaker.config.consecFaults = 2;
-      circuitBreaker
-        .findOrCreate('geo', breaker.defaultConfig())
-        .then((records) => {
-          var record = records[0];
-          circuitBreaker
-            .incrementFaults(record)
-            .then(() => { done(); });
-        });
+      breaker = new PromiseBreaker('geo', { consecFaults: 2 });
+      breaker.setup()
+        .then(() => breaker.store.get('geo'))
+        .then((record) => breaker.incrementFaults(record))
+        .then(() => done());
     });
 
     it('attributes are stored on the record', function(done) {
@@ -162,9 +161,9 @@ describe('PromiseBreaker', function() {
         .run(attributes)
         .catch(assertBreakerTripped);
 
-      function assertBreakerTripped() {
-        circuitBreaker
-          .findByName(breaker.name)
+      function assertBreakerTripped(response) {
+        breaker.store
+          .get(breaker.name)
           .then(function(record) {
             assert.equal(record.fault_ct, 2);
             assert.equal(record.consec_faults, 2);
@@ -179,17 +178,14 @@ describe('PromiseBreaker', function() {
   describe('when the breaker is tripped because of too many faults within the window', function() {
     beforeEach(function(done) {
       breaker = new PromiseBreaker('geo');
-      circuitBreaker
-        .findOrCreate('geo', breaker.defaultConfig())
-        .then((records) => {
-          var record = records[0];
-          circuitBreaker
-            .testUpdate(record, {
-              fault_ct: breaker.config.windowFaults - 1,
-              fault_ts: Date.now() - breaker.config.faultWindow + 75
-            })
-            .then(() => { done(); });
-        });
+      breaker.setup()
+        .then((record) => {
+          return breaker.store.set(this.name, _.extend(record, {
+            fault_ct: breaker.config.windowFaults - 1,
+            fault_ts: Date.now() - breaker.config.faultWindow + 75
+          }));
+        })
+        .then(() => done());
     });
 
     it('attributes are stored on the record', function(done) {
@@ -201,8 +197,8 @@ describe('PromiseBreaker', function() {
         .catch(assertBreakerTripped);
 
       function assertBreakerTripped() {
-        circuitBreaker
-          .findByName(breaker.name)
+        breaker.store
+          .get(breaker.name)
           .then(function(record) {
             assert.equal(record.fault_ct, breaker.config.windowFaults);
             assert(record.tripped);
@@ -216,14 +212,9 @@ describe('PromiseBreaker', function() {
   describe('when the breaker has tripped', function() {
     beforeEach(function(done) {
       breaker = new PromiseBreaker('geo');
-      circuitBreaker
-        .findOrCreate('geo', breaker.defaultConfig())
-        .then((records) => {
-          var record = records[0];
-          circuitBreaker
-            .trip(record)
-            .then(() => { done(); })
-        })
+      breaker.setup()
+        .then((record) => breaker.trip(record))
+        .then(() => { done(); });
     });
 
     it('will fail fast', function(done) {
@@ -244,28 +235,22 @@ describe('PromiseBreaker', function() {
   describe('when the breaker is tripped, but the window has expired', function() {
     beforeEach(function(done) {
       breaker = new PromiseBreaker('geo');
-      circuitBreaker
-        .findOrCreate('geo', breaker.defaultConfig())
-        .then((records) => {
-          var record = records[0];
-          circuitBreaker
-            .testUpdate(record, {
-              fault_ct: 5,
-              consec_faults: 3,
-              tripped: true,
-              trip_ts: Date.now() - breaker.config.faultWindow - 100,
-              fault_ts: Date.now() - breaker.config.faultWindow - 100
-            })
-            .then(() => { done(); })
-        })
+      breaker.setup()
+        .then(() => breaker.store.set(breaker.name, {
+          fault_ct: 5,
+          consec_faults: 3,
+          tripped: true,
+          trip_ts: Date.now() - breaker.config.faultWindow - 100,
+          fault_ts: Date.now() - breaker.config.faultWindow - 100 }))
+        .then(() => { done(); });
     });
 
     it('restores values on the record', function(done) {
       breaker
         .run(attributes)
-        .then((response) => {
-          circuitBreaker
-            .findByName(breaker.name)
+        .then(() => {
+          breaker.store
+            .get(breaker.name)
             .then((record) => {
               assert.equal(record.fault_ct, 0);
               assert.equal(record.consec_faults, 0);
@@ -288,4 +273,3 @@ describe('PromiseBreaker', function() {
     });
   });
 });
-
